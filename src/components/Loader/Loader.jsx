f96d6daaf533975941a89d51ed753a411c7fc6f6
skylerseifert <skylerseifert@gmail.com>
2020-10-14 20:29:14 -0700

    Update Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 
 import './Loader.css';
 
-const Container = styled.div`
-    display: flex;
-    justify-content: center;
-`;
-
 export default function Loader() {
     return (
-        <Container>
+        <div className={'Loader-container'}>
             <div className={'Loader'} />
-        </Container>
+        </div>
     );
 }
