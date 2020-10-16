ff092b63af22ec3b50483c33c5cf86e247bc9921
skylerseifert <skylerseifert@gmail.com>
2020-10-16 13:09:56 -0700

    Refactor and Readme

--- seed.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-require('./config/database');
-const Column = require('./models/column');
-
-Column.find({}, function (err, columns) {
-    columns[0].tasks.push({ content: 'Feed Dogs' });
-    columns[0].save();
-});
