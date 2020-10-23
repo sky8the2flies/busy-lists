00f3ead979aa5610f738a48ccab79425bc215f54
skylerseifert <skylerseifert@gmail.com>
2020-10-22 21:39:20 -0700

    Update backend

--- controllers/api/images.js
+++ controllers/api/images.js
@@ -5,13 +5,12 @@ module.exports = {
     getRandom,
 };
 
-const BASE_URL = `https://api.unsplash.com/photos/?client_id=${process.env.UNSPLASH_KEY}&orientation=landscape&per_page=30&page=1`;
+const BASE_URL = `https://api.unsplash.com/search/photos/?client_id=${process.env.UNSPLASH_KEY}&query=cyberpunk&orientation=landscape&per_page=25`;
 
 async function getAll(req, res) {
     return request(BASE_URL, function (err, response, body) {
-        console.log(err);
-        console.log(body);
-        return res.json(body);
+        if (!err) return res.status(200).json(JSON.parse(body));
+        return res.status(400).json(JSON.parse(body));
     });
 }
 
@@ -20,7 +19,6 @@ const BASE_URL_RANDOM = `https://api.unsplash.com/photos/random/?client_id=${pro
 function getRandom(req, res) {
     return request(BASE_URL_RANDOM, function (err, response, body) {
         console.log(err);
-        console.log(body);
         return res.json(body);
     });
 }
